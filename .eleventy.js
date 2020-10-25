fc489254bf6071c02368c1746bcb2a49c0103664
Arron McCrory <arron@WINDOWS-DESKTOP-ARRON.localdomain>
2020-10-25 15:19:07 -0700

    added ability to have gallery-folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,30 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('images')
-    eleventyConfig.addPassthroughCopy('assets')
+const fg = require("fast-glob");
+// Run search for images in /gallery
+const galleryImages = fg.sync(["**/gallery/*", "!**/_site"]);
 
-    eleventyConfig.addPassthroughCopy('admin')
-  
-    const { DateTime } = require("luxon");
-  
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("assets");
 
-    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-      eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-        return DateTime.fromJSDate(dateObj, {
-          zone: 'utc'
-        }).toFormat('yy-MM-dd');
-      });
-  
-      eleventyConfig.addFilter("readableDate", dateObj => {
-      return DateTime.fromJSDate(dateObj, {
-        zone: 'utc'
-      }).toFormat("dd-MM-yy");
-    });
-  
-  };
+  eleventyConfig.addPassthroughCopy("admin");
+
+  const { DateTime } = require("luxon");
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: "utc",
+    }).toFormat("yy-MM-dd");
+  });
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: "utc",
+    }).toFormat("dd-MM-yy");
+  });
+
+  //Create collection of gallery images
+  eleventyConfig.addCollection("gallery", function (collection) {
+    return galleryImages;
+  });
+};
